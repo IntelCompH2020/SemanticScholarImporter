9cd1f723934bf00606a345afe8d90ce6df5b60b9
jeroarenas <jeronimo.arenas@gmail.com>
2021-11-11 01:41:46 +0100

    Lemas and indexes not created during DB initialization. Modified Database Tables drop

--- dbManager/create_sql.sql
+++ dbManager/create_sql.sql
@@ -142,9 +142,9 @@ CREATE TABLE "citations"(
     FOREIGN KEY ("S2paperID2") REFERENCES "S2papers" ("S2paperID") ON DELETE CASCADE
 );
 
-CREATE INDEX S2idPaper ON "S2papers" ("S2paperID");
-CREATE INDEX S2idAuth ON "S2authors" ("S2authorID");
-CREATE INDEX paper1 ON "citations" ("S2paperID1");
-CREATE INDEX paper2 ON "citations" ("S2paperID2");
-CREATE INDEX paper ON "paperAuthor" ("S2paperID");
-CREATE INDEX author ON "paperAuthor" ("S2authorID");
+-- CREATE INDEX S2idPaper ON "S2papers" ("S2paperID");
+-- CREATE INDEX S2idAuth ON "S2authors" ("S2authorID");
+-- CREATE INDEX paper1 ON "citations" ("S2paperID1");
+-- CREATE INDEX paper2 ON "citations" ("S2paperID2");
+-- CREATE INDEX paper ON "paperAuthor" ("S2paperID");
+-- CREATE INDEX author ON "paperAuthor" ("S2authorID");
