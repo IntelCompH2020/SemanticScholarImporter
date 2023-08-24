133cbd4bba4e91038928268b84789039d5b02152
jeroarenas <jeronimo.arenas@gmail.com>
2023-08-24 19:52:16 +0200

    Removing files not compatible with new version of Semantic Scholar

--- dbManager/create_index.sql
+++
@@ -1,6 +0,0 @@
-CREATE INDEX S2idPaper ON "S2papers" ("S2paperID");
-CREATE INDEX S2idAuth ON "S2authors" ("S2authorID");
-CREATE INDEX paper1 ON "citations" ("S2paperID1");
-CREATE INDEX paper2 ON "citations" ("S2paperID2");
-CREATE INDEX paper ON "paperAuthor" ("S2paperID");
-CREATE INDEX author ON "paperAuthor" ("S2authorID");
